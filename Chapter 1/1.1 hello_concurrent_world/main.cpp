419ece7758294cc9f1bdaea90af8d7053f2bf34a
Vinicius Sa <viniciusjavs@gmail.com>
2020-10-28 09:30:09 -0300

    Add Hello Concurrent World

---
+++ Chapter 1/1.1 hello_concurrent_world/main.cpp
@@ -0,0 +1,14 @@
+/*
+  Listing 1.1
+  A simple Hello Concurrent World program
+*/
+
+#include <iostream>
+#include <thread>
+
+void hello() { std::cout << "Hello Concurrent World\n"; }
+
+int main() {
+  std::thread t{hello};
+  t.join();
+}
