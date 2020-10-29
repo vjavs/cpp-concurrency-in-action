6fe5ce07c5ee3d874d6a65828f7a58d8aa4fb751
Vinicius Sa <viniciusjavs@gmail.com>
2020-10-29 02:01:11 -0300

    Chapter 2: Add listing 2.9

---
+++ Chapter 2/2.9 parallel std::accumulate/main.cpp
@@ -0,0 +1,42 @@
+/*
+  Listing 2.9
+  A naive parallel version of std::accumulate
+*/
+
+#include <numeric>
+#include <thread>
+#include <vector>
+
+template <typename Iterator, typename T> struct accumulate_block {
+  void operator()(Iterator first, Iterator last, T &result) {
+    result = std::accumulate(first, last, result);
+  }
+};
+
+template <typename Iterator, typename T>
+T parallel_accumulate(Iterator first, Iterator last, T init) {
+  auto const length = std::distance(first, last);
+  if (!length)
+    return init;
+  auto const min_per_thread = 25ul;
+  auto const max_threads = (length + min_per_thread - 1) / min_per_thread;
+  auto const hardware_threads = std::thread::hardware_concurrency();
+  auto const num_threads =
+      std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+  auto const block_size = length / num_threads;
+  std::vector<T> results(num_threads);
+  std::vector<std::thread> threads(num_threads - 1);
+  Iterator block_start{first};
+  for (auto i = 0ul; i < (num_threads - 1); ++i) {
+    Iterator block_end = block_start;
+    std::advance(block_end, block_size);
+    threads[i] = std::thread(accumulate_block<Iterator, T>(), block_start,
+                             block_end, std::ref(results[i]));
+    block_start = block_end;
+  }
+  accumulate_block<Iterator, T>()(block_start, last, results[num_threads - 1]);
+
+  for (auto &entry : threads)
+    entry.join();
+  return std::accumulate(results.begin(), results.end(), init);
+}
